dbcdac2afe2d04f689bd52fe47d0ae0a24349e8f
Abhay Shekhawat <shekhawat.abhay94@gmail.com>
2020-10-09 21:33:59 +0530

    built sign in page and the reusable form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { FaqsContainer } from './containers/faqs';
-import { FooterContainer } from './containers/footer';
-import { JumbotronContainer } from './containers/jumbotron';
 import * as ROUTES from './constants/routes';
 import {BrowserRouter as Router,Route, Switch} from 'react-router-dom';
 import {Home,Browse,Signin,Signup} from './pages';
--- src/containers/header.js
+++ src/containers/header.js
@@ -8,7 +8,7 @@ export default function HeaderContainer({children}) {
         <Header>
             <Header.Frame>
                 <Header.Logo to = {ROUTES.HOME} alt ='NETFLIX' src = {logo}/>
-                <Header.ButtonLink>Sign In</Header.ButtonLink>
+                <Header.ButtonLink to = {ROUTES.SIGN_IN} >Sign In</Header.ButtonLink>
             </Header.Frame>
             {children}
         </Header>
